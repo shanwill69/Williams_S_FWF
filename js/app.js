e1ed12b643880d36c99179b5a5508a459d529396
Shannon <shanwill69@fullsail.com>
2015-08-19 11:40:15 -0500

    corrected, up and running

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 var theApp = angular.module("MyApp",[]);
 theApp.controller("NewController", function($scope,dataService){
 
-    $scope.addresses = dataService.getItem();
+    $scope.addresses = dataService.getAdds();
         $scope.newAdds = {};
 
     $scope.addNewAdds = function(){
-        dataService.saveAdds($scope.newAdds.name,$scope.newAdds.street,
+        dataService.saveAdds($scope.newAdds.myname,$scope.newAdds.street,
             $scope.newAdds.city,$scope.newAdds.state,$scope.newAdds.zip);
         $scope.newAdds = {};
     };
 
     $scope.removeAdds = function(idx){
         dataService.removeAddsAt(idx);
-    };
+    }
 
     $scope.clearIt = function(){
         dataServiceataService.destroyLocalStorage();
--- js/dataService.js
+++ js/dataService.js
@@ -6,24 +6,24 @@ angular.module("MyApp").service("dataService",function(){
     var addressArray = [];
 
     this.getAdds = function(){
-        var addsArray =JSON.parse(localStorage.getItem("addressesLS")) ||[];
+        var addsArray =JSON.parse(localStorage.getItem("addressesLS")) || [];
         addressArray = addsArray;
             console.log(addsArray);
-        return addsArray;
+        return addressArray;
 
     }
 
     this.saveAdds = function(pName,pAddress,pCity,pState,pZip){
         var savedAdds = {name: pName, address: pAddress, city:pCity, state: pState, zip: pZip };
-        addsArray.push(savedAdds);
-        localStorage.setItem("addressesLS",JSON.stringify(addsArray));
+        addressArray.push(savedAdds);
+        localStorage.setItem("addressesLS",JSON.stringify(addressArray));
     }
     this.removeAddsAt = function(pIndex){
-        addsArray.splice(pIndex,1);
-        localStorage.setItem("addressesLS",JSON.stringify(addsArray));
+        addressArray.splice(pIndex,1);
+        localStorage.setItem("addressesLS",JSON.stringify(addressArray));
     }
     this.destroyLocalStorage = function(){
-        addsArray.splice(0);
+        addressArray.splice(0);
         localStorage.clear();
     }
 
